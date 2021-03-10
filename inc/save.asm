e4bcea4ff20e29cc36d0de32b560ff54ca7ffdff
Donald Hays <donald.hays@gmail.com>
2021-03-09 17:47:16 -0800

    SRAM mode control doesn't apply in this game.

--- inc/save.asm
+++ inc/save.asm
@@ -12,12 +12,8 @@ sram_record_size EQU $0002
 sram_disable EQU $00
 sram_enable EQU $0A
 
-rom_sram_mode_rom EQU $00
-rom_sram_mode_sram EQU $01
-
 sram_enable_reg EQU $0000
 sram_bank_number_reg EQU $4000
-rom_sram_mode_reg EQU $6000
 
 SECTION "Save", ROM0
 magicData:
@@ -56,9 +52,6 @@ eraseSRAM:
 enableSRAM:
   ld a, sram_enable
   ld [sram_enable_reg], a
-  
-  ld a, rom_sram_mode_sram
-  ld [rom_sram_mode_reg], a
   
   ld a, 0
   ld [sram_bank_number_reg], a
